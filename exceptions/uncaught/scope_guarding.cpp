49c3a18d3c63f4cf6aa4212a7d46188f4c3ae2b3
monpeco <monpeco@gmail.com>
2018-04-11 03:04:16 +0000

    Add VecScopedGuard class definition

--- exceptions/uncaught/scope_guarding.cpp
+++ exceptions/uncaught/scope_guarding.cpp
@@ -4,11 +4,29 @@
 using namespace std;
 
 template<class TContainer>
-class VecScoped{
+class VecScopedGuard{
+public:
+  VecScopedGuard(TContainer* vecptr)
+    : engaged_(true), vecptr_(vecptr){ // Assign vecptr and set guard to true
+  }
 
+  ~VecScopedGuard(){
+    if(engaged_){    // Remove last vector entry
+      vecptr_->pop_back();
+    }
+  }
+
+  void disengage(){
+    engaged_ = false;   // Vectors updated so release guard
+  }
+private:
+  bool engaged_;
+  TContainer* vecptr_;
 };
 
 int main(){
+  vector<string> firstVector;
+  vector<string> secondVector;
 
   return 0;
 }
